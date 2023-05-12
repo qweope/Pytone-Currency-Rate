80801b171b3fa8cd2c75df798296fd463217e7c9
LilqLevI <arsenij.matusenko07@gmail.com>
2023-05-12 13:16:50 +0300

    upd

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_rate(currency_from: str, currency_to: str) -> float:
 
 if __name__ == "__main__":
     amount = float(input('Enter the amount:'))
-    currency_from = str(input('Enter initial currency:')).strip()
-    currency_to = str(input('Enter desired currency:')).strip()
+    currency_from = str(input('Enter initial currency:')).strip().upper()
+    currency_to = str(input('Enter desired currency:')).strip().upper()
     rate = get_rate(currency_from, currency_to)
     print(f'Converted {amount} {currency_from} with rate {rate} equals to {amount*rate} {currency_to}')
