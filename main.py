ed6e2320dc48c2b79f186860a8e230b46c27153d
LilqLevI <arsenij.matusenko07@gmail.com>
2023-05-12 12:45:07 +0300

    Currency-Rate
    With this you can find out the current exchange rate

---
+++ main.py
@@ -0,0 +1,14 @@
+from forex_python.converter import CurrencyRates
+
+
+def get_rate(currency_from: str, currency_to: str) -> float:
+    currency_rates = CurrencyRates()
+    return currency_rates.get_rate(currency_from, currency_to)
+
+
+if __name__ == "__main__":
+    amount = float(input('Enter the amount:'))
+    currency_from = str(input('Enter initial currency:')).strip()
+    currency_to = str(input('Enter desired currency:')).strip()
+    rate = get_rate(currency_from, currency_to)
+    print(f'Converted {amount} {currency_from} with rate {rate} equals to {amount*rate} {currency_to}')
